091715911420f6db64ee1dd00a37a1b6ac697a27
Yves Zoundi <yveszoundi@gmail.com>
2014-07-13 14:15:03 -0400

    Problems 10 to 15.

---
+++ p12.clj
@@ -0,0 +1,13 @@
+;; 4Clojure Question 12
+;;
+;; All Clojure collections support sequencing.  You can operate on sequences with functions like first, second, and last.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+;; (= __ (first '(3 2 1)))
+
+;; (= __ (second [2 3 4]))
+
+;; (= __ (last (list 1 2 3)))
+
+(= 3 (first '(3 2 1)))
---
+++ p13.clj
@@ -0,0 +1,8 @@
+;; 4Clojure Question 13
+;;
+;; The rest function will return all the items of a sequence except the first.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+;; (= __ (rest [10 20 30 40]))
+(= [20 30 40] (rest [10 20 30 40]))
